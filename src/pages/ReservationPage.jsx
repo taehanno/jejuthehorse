4e5d3da27a030875dd7d306250cd612557266919
taehanno <kaistwill@gmail.com>
2026-04-03 16:23:46 +0900

    feat: 시간 선택 30분 단위로 변경

--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -6,7 +6,17 @@ import SummarySection from '../components/SummarySection'
 import Footer from '../components/Footer'
 import { calcPersonPrice, formatPrice, buildPersonsDetail } from '../utils/pricing'
 
-const TIME_OPTIONS = ['09:00', '10:00', '11:00', '13:30', '14:00', '15:00', '16:00', '17:00']
+const TIME_OPTIONS = [
+  '09:00', '09:30',
+  '10:00', '10:30',
+  '11:00', '11:30',
+  // 12:00~13:20 휴게
+  '13:30',
+  '14:00', '14:30',
+  '15:00', '15:30',
+  '16:00', '16:30',
+  '17:00', '17:30',
+]
 
 function isMonday(dateStr) {
   if (!dateStr) return false
